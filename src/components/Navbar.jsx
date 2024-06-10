bf835eef1f01f4211a05cd3bf57de84f02cb1ff0
kvnallenn <kvnallenn@gmail.com>
2024-06-10 17:01:59 +0700

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,7 @@
 import { HamburgerIcon } from '@chakra-ui/icons';
-import { Box, ChakraProvider, Flex, Spacer, Text, HStack } from '@chakra-ui/react'
+import { Box, ChakraProvider, Flex, Spacer, Text, HStack, IconButton } from '@chakra-ui/react'
 import '@fontsource/poppins';
 import '@fontsource/secular-one';
-import { IconButton } from '@chakra-ui/react';
 
 
 
@@ -10,6 +9,7 @@ const Navbar = () =>{
   return(
     <>
     <ChakraProvider>
+    <HStack display={{ base: 'none', lg: 'flex' }} spacing={10} >
     <Box bg='#E4E4E4' w='100%' color='white' h="42px" p={3}>
       <Flex alignItems='center' gap={7} mr={10}>
       <Spacer />
@@ -17,29 +17,26 @@ const Navbar = () =>{
       <Text color={'black'} fontSize={12} fontFamily={'poppins'}>Hubungi Kami</Text>
       </Flex>
     </Box>
+    </HStack>
     <Box w='100%' color='white' h="99px" p={6}>
     <Flex alignItems='center' gap={7} mr={7}>
       <Text fontFamily={'Secular One'} fontSize={32} color={'black'} ml={15}>YOUNIQUE</Text>
-      <HStack display={{ base: 'none', md: 'flex' }} spacing={10}>
+      <HStack display={{ base: 'none', lg: 'flex' }} spacing={10}>
         <Text color={'black'} fontSize={14} fontFamily={'poppins'} fontWeight={'bold'} ml={5}>Shop</Text>
         <Text color={'black'} fontSize={14} fontFamily={'poppins'} fontWeight={'bold'}>Promo</Text>
         <Text color={'black'} fontSize={14} fontFamily={'poppins'} fontWeight={'bold'}>Kategori</Text>
         <Text color={'black'} fontSize={14} fontFamily={'poppins'} fontWeight={'bold'}>Jastip</Text>
       </HStack>
       <Spacer />
-      <HStack display={{ base: 'none', md: 'flex' }} spacing={10}>
+      <HStack display={{ base: 'none', lg: 'flex' }} spacing={10}>
       <Text color={'black'} fontSize={15} fontFamily={'poppins'} >Search</Text>
       <Text color={'black'} fontSize={15} fontFamily={'poppins'} >Masuk</Text>
       <Text color={'black'} fontSize={15} fontFamily={'poppins'} >Keranjang</Text>
       <Text color={'black'} fontSize={15} fontFamily={'poppins'}>Wishlist</Text>
       </HStack>
-          <IconButton
-            size="md"
-            icon={<HamburgerIcon />}
-            aria-label="Open Menu"
-            display={{ md: 'none' }}
-            onClick={onOpen}
-          />
+      <HStack display={{ base: 'flex', lg: 'none' }} spacing={10}>
+      <IconButton aria-label='Search database' icon={<HamburgerIcon />} />
+      </HStack>
       </Flex>
     </Box>
     </ChakraProvider>
