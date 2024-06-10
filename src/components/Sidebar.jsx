c807d499f8517093caf11dae5f33ad4f1b19c5cd
kvnallenn <kvnallenn@gmail.com>
2024-06-11 00:10:04 +0700

    Pencarian

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { HamburgerIcon } from "@chakra-ui/icons";
-import { useDisclosure, IconButton, Drawer, DrawerOverlay, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerBody } from "@chakra-ui/react";
+import { useDisclosure, IconButton, Drawer, DrawerOverlay, DrawerCloseButton, DrawerContent, DrawerHeader, DrawerBody, VStack, Text, DrawerFooter, Button, HStack, Input } from "@chakra-ui/react";
 
 const Sidebar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -26,9 +26,21 @@ const Sidebar = () => {
         <DrawerContent>
           <DrawerCloseButton />
           <DrawerHeader>YOUNIQUE.LY</DrawerHeader>
-
-          <DrawerBody>halo coki</DrawerBody>
-        </DrawerContent>
+          <DrawerBody>
+          <VStack align='stretch'>
+            <Text color={"black"} fontSize={15} fontFamily={"poppins"}>
+                Promo
+            </Text>
+            <Text color={"black"} fontSize={15} fontFamily={"poppins"} mt={2}>
+                Kategori
+            </Text>
+            <Text color={"black"} fontSize={15} fontFamily={"poppins"} mt={2}>
+                Jastip
+            </Text>
+            <Input variant='flushed' fontSize={15} fontFamily={"poppins"} mt={1} placeholder='Cari barang disini...'/>
+          </VStack>
+          </DrawerBody>
+        </DrawerContent>              
       </Drawer>
     </>
   );
