e916b2a29dbfc088fa49bad6b8b602a1bc100443
Kevin Allen <kvnallenn@gmail.com>
2024-07-12 12:27:42 +0700

    New Hero Update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,10 @@ import {
   Center,
   Container,
   Flex,
+  Grid,
   HStack,
   Image,
+  VStack,
 } from "@chakra-ui/react";
 
 const Hero = () => {
@@ -13,7 +15,7 @@ const Hero = () => {
     <>
       <Container>
         <Center>
-          <HStack>
+          <VStack>
             <Box position="relative" display="inline-block">
               <Image src="../img/53567251.png" />
               <Button
@@ -31,7 +33,55 @@ const Hero = () => {
                 Shop Now
               </Button>
             </Box>
-          </HStack>
+            <Box position="relative" display="inline-block">
+              <Image src="../img/53567252.png" />
+              <HStack spacing={10}>
+                <Box position="absolute" transform="translate(-50%, -50%)" top="63%"
+                left="50%" id="box-hero">
+                <Button
+                  backgroundColor="#303144"
+                  color="#FFFFFF"
+                  width="88px"
+                  height="31px"
+                  fontFamily={"poppins"}
+                  id="button-shop-2"
+                >
+                  T-Shirt
+                </Button>
+                <Button
+                  backgroundColor="#303144"
+                  color="#FFFFFF"
+                  width="88px"
+                  height="31px"
+                  fontFamily={"poppins"}
+                  id="button-shop-2"
+                >
+                  One Set
+                </Button>
+                <Button
+                  backgroundColor="#303144"
+                  color="#FFFFFF"
+                  width="88px"
+                  height="31px"
+                  fontFamily={"poppins"}
+                  id="button-shop-2"
+                >
+                  Celana
+                </Button>
+                <Button
+                  backgroundColor="#303144"
+                  color="#FFFFFF"
+                  width="88px"
+                  height="31px"
+                  fontFamily={"poppins"}
+                  id="button-shop-2"
+                >
+                  Dress
+                </Button>
+                </Box>
+              </HStack>
+            </Box>
+          </VStack>
         </Center>
       </Container>
     </>
